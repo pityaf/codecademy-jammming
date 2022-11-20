8f8760f0863857699a8ec9b9d74f3d976bf1a5f0
Paolo Cantoreggi <paolo.cantoreggi@gmail.com>
2022-11-20 21:34:16 +0100

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      <div class="App">
+          {/*Add a SearchBar component*/} 
+        <div class="App-playlist">
+          {/*Add a SearchResults component*/}
+          {/*Add a Playlist component*/}
+        </div>
+      </div>
+    </div>
+  );
+}
